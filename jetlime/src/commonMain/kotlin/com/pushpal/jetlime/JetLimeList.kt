dba1179efc85c3e0b7b20679d60b9f262be76b42
Steyn Geldenhuys <Phaestion@users.noreply.github.com>
2025-12-02 14:23:36 +0100

    Add `reverseLayout` parameter to `JetLimeList` LazyColumn and LazyRow

--- jetlime/src/commonMain/kotlin/com/pushpal/jetlime/JetLimeList.kt
+++ jetlime/src/commonMain/kotlin/com/pushpal/jetlime/JetLimeList.kt
@@ -72,6 +72,7 @@ import com.pushpal.jetlime.Arrangement.VERTICAL
  * @param listState The state object to be used for the LazyColumn.
  * @param contentPadding The padding to apply to the content inside the LazyColumn.
  * @param key A factory of stable and unique keys representing the item. Using the same key for multiple items in the list is not allowed. Type of the key should be saveable via Bundle on Android. If null is passed the position in the list will represent the key.
+ * @param reverseLayout Whether to reverse the layout direction of the LazyColumn - new events to the top of the column
  * @param itemContent A composable lambda that takes an index, an item of type [T], and an [EventPosition] to build each item's content.
  */
 @Composable
@@ -82,13 +83,14 @@ fun <T> JetLimeColumn(
   listState: LazyListState = rememberLazyListState(),
   contentPadding: PaddingValues = PaddingValues(0.dp),
   key: ((index: Int, item: T) -> Any)? = null,
+  reverseLayout: Boolean = false,
   itemContent: @Composable (index: Int, T, EventPosition) -> Unit,
 ) {
   CompositionLocalProvider(LocalJetLimeStyle provides style.alignment(VERTICAL)) {
     LazyColumn(
       modifier = modifier,
       state = listState,
-      reverseLayout = false,
+      reverseLayout = reverseLayout,
       verticalArrangement = Arrangement.Top,
       horizontalAlignment = Alignment.Start,
       flingBehavior = ScrollableDefaults.flingBehavior(),
@@ -137,6 +139,7 @@ fun <T> JetLimeColumn(
  * @param listState The state object to be used for the LazyRow.
  * @param contentPadding The padding to apply to the content inside the LazyRow.
  * @param key A factory of stable and unique keys representing the item. Using the same key for multiple items in the list is not allowed. Type of the key should be saveable via Bundle on Android. If null is passed the position in the list will represent the key.
+ * @param reverseLayout Whether to reverse the layout direction of the LazyRow - new events to the left (start) of the row
  * @param itemContent A composable lambda that takes an index, an item of type [T], and an [EventPosition] to build each item's content.
  */
 @Composable
@@ -147,13 +150,14 @@ fun <T> JetLimeRow(
   listState: LazyListState = rememberLazyListState(),
   contentPadding: PaddingValues = PaddingValues(0.dp),
   key: ((index: Int, item: T) -> Any)? = null,
+  reverseLayout: Boolean = false,
   itemContent: @Composable (index: Int, T, EventPosition) -> Unit,
 ) {
   CompositionLocalProvider(LocalJetLimeStyle provides style.alignment(HORIZONTAL)) {
     LazyRow(
       modifier = modifier,
       state = listState,
-      reverseLayout = false,
+      reverseLayout = reverseLayout,
       horizontalArrangement = Arrangement.Start,
       verticalAlignment = Alignment.Top,
       flingBehavior = ScrollableDefaults.flingBehavior(),
